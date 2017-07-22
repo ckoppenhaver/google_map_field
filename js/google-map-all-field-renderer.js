ed10b943e365a8b45af60d7a1545633a8c8e0840
Courtney Koppenhaver <ckoppenhaver@gmail.com>
2017-07-22 16:17:33 -0700

    Adding in change region dropdown.

--- js/google-map-all-field-renderer.js
+++ js/google-map-all-field-renderer.js
@@ -5,106 +5,117 @@ var google_map_field_map;
 
   Drupal.behaviors.google_map_all_field_renderer = {
     attach: function (context, settings) {
-      console.log('wheee');
 
       $('.google-map-field.map-all .map-container').once('.google-map-field-processed').each(function(index, item) {
-        // Get the settings for the map from the Drupal.settings object.
-        var routeAllArray = [];
-        var markerAllArray = [];
-        var indexTotal = 0;
-
-        $.each(settings.google_map_field.all.marker, function(markerLoopIndex, markerLoopItems){
-          markerTemp = toObj(settings.google_map_field.all.marker[markerLoopIndex]);
-          $.each(markerTemp, function(markerLoopItemIndex, markerLoopItem){
-            markerAllArray[indexTotal] = markerLoopItem[0];
-            indexTotal++;
-          });
-        });
-
-        indexTotal = 0;
-        $.each(settings.google_map_field.all.route, function(routeLoopItemsIndex, routeLoopItems){
-          routeTemp = toObj(settings.google_map_field.all.route[routeLoopItemsIndex]);
-          $.each(routeTemp, function(routeLoopItemIndex, routeLoopItem){
-            routeAllArray[indexTotal] = routeLoopItem;
-            indexTotal++;
-          });
-        });
-
-        var lat = $(this).attr('data-lat');
-        var lon = $(this).attr('data-lon');
-        var zoom = parseInt($(this).attr('data-zoom'));
-        var type = $(this).attr('data-type');
-        var show_controls = $(this).attr('data-controls-show') === "true";
-        var routeIndex = 0;
-        var flightPathArray = [];
-        var routeCoords = routeAllArray;
-        var markerCoords = markerAllArray;
-        var markerArray = [];
-        var markerIndex = 0;
-        var markerEditIndex = 0;
-        var infoWindow = [];
-
-        // Create the map coords and map options.
-        var latlng = new google.maps.LatLng(lat, lon);
-        var mapOptions = {
-          zoom: zoom,
-          center: latlng,
-          streetViewControl: false,
-          mapTypeId: type,
-          disableDefaultUI: show_controls ? false : true,
-        };
-
-        var google_map_field_map = new google.maps.Map(this, mapOptions);
-
-        google.maps.event.trigger(google_map_field_map, 'resize');
-
-        routeCoords.forEach(function(path, index) {
-          routeIndex = index;
-          flightPathArray[routeIndex] = new google.maps.Polyline({
-            path: path,
-            geodesic: true,
-            strokeColor: path[0].color,
-            strokeOpacity: 1.0,
-            strokeWeight: path[0].size
-          });
-          flightPathArray[routeIndex].setMap(google_map_field_map);
-        });
-
-        markerCoords.forEach(function(path, index) {
-          markerIndex = index;
-          markerEditIndex = index;
-
-          infoWindow[index] = new google.maps.InfoWindow({
-            content: path.notes
-          });
-
-          // drop a marker at the specified lat/lng coords
-          markerArray[index] = new google.maps.Marker({
-            position: path,
-            optimized: false,
-            draggable: false,
-            visible: true,
-            map: google_map_field_map,
-            icon: path.flag
-          });
-
-          markerArray[index].addListener('click', function() {
-            infoWindow[index].open(google_map_field_map, markerArray[index]);
-          });
-        });
+        paintMap(context, settings, this);
       });
 
+      $('#region-selector', context).on('change', function(){
+        // console.log('ran');
+        $(document, context).trigger('region-change');
+        var mapContainer = $(this).parent().find('.map-container')[0];
+        paintMap(context, settings, mapContainer);
+      });
     }
   };
 
-  toObj = function(thing) {
+  paintMap = function(context, settings, currentMap) {
+    // Get the settings for the map from the Drupal.settings object.
+    var google_map_field_settings = settings.google_map_field.all.config;
+    var lat = $(currentMap).attr('data-lat');
+    var lon = $(currentMap).attr('data-lon');
+    var zoom = parseInt($(currentMap).attr('data-zoom'));
+    var type = $(currentMap).attr('data-type');
+    var show_controls = $(currentMap).attr('data-controls-show') === "true";
+    var regionSelected = $('#region-selector').val();
+    var markerArray = [];
+    var infoWindow = [];
+    var markerIndex = 0;
+    var routeObj = [];
+
+
+// Create the map coords and map options.
+    var latlng = new google.maps.LatLng(lat, lon);
+    var mapOptions = {
+      zoom: zoom,
+      center: latlng,
+      streetViewControl: false,
+      mapTypeId: type,
+      disableDefaultUI: !show_controls
+    };
+
+    // Initial Map Setup.
+    var google_map_field_map = new google.maps.Map(currentMap, mapOptions);
+    google.maps.event.trigger(google_map_field_map, 'resize');
+
+    // If routes are enabled gather data and display the routes
+    if (google_map_field_settings.route) {
+      $.each(settings.google_map_field.all.route, function (routeLoopItemsIndex) {
+        if (settings.google_map_field.all.regions[routeLoopItemsIndex] === regionSelected
+          || regionSelected === 'all'
+          || regionSelected === null
+        ) {
+          routeObj = toObj(settings.google_map_field.all.route[routeLoopItemsIndex]);
+
+          $.each(routeObj, function (routeLoopItemIndex, routeLoopItem) {
+            new google.maps.Polyline({
+              path: routeLoopItem,
+              geodesic: true,
+              strokeColor: routeLoopItem[0].color,
+              strokeOpacity: 1.0,
+              strokeWeight: routeLoopItem[0].size
+            }).setMap(google_map_field_map);
+          });
+        }
+      });
+    }
+
+    // If Markers are enabled gather data and display the markers.
+    if (google_map_field_settings.marker) {
+      $.each(settings.google_map_field.all.marker, function (markerLoopIndex) {
+        if (settings.google_map_field.all.regions[markerLoopIndex] === regionSelected
+          || regionSelected === 'all'
+          || regionSelected === null
+        ) {
+          markerTemp = toObj(settings.google_map_field.all.marker[markerLoopIndex]);
+          $.each(markerTemp, function (markerLoopItemIndex, markerLoopItem) {
+            if (google_map_field_settings.marker) {
+
+              // drop a marker at the specified lat/lng coords
+              markerArray[markerIndex] = new google.maps.Marker({
+                position: markerLoopItem[0],
+                optimized: false,
+                draggable: false,
+                visible: true,
+                map: google_map_field_map,
+                icon: markerLoopItem[0].flag,
+                markerIndex: markerIndex
+              });
+
+              if (google_map_field_settings.tooltip && markerLoopItem[0].notes !== '') {
+                infoWindow[markerIndex] = new google.maps.InfoWindow({
+                  content: markerLoopItem[0].notes
+                });
+                markerArray[markerIndex].addListener('click', function () {
+                  infoWindow[this.markerIndex].open(google_map_field_map, this);
+                });
+              }
+              markerIndex++;
+            }
+          });
+        }
+      });
+    }
+  };
+
+  toObj = function(string) {
     var returnObj = [];
     try {
-      thing = JSON.parse(thing);
-      thing = $.map(thing, function(value, index) {
+      string = JSON.parse(string);
+      string = $.map(string, function(value, index) {
         return [value];
       });
-      thing.forEach(function (pathSet, pathIndex){
+      string.forEach(function (pathSet, pathIndex){
         pathSet.forEach(function(pathCoords, pathCoordsIndex) {
           pathSet[pathCoordsIndex] = JSON.parse(pathCoords);
         });
--- src/Plugin/Block/GoogleMapAllPointsBlock.php
+++ src/Plugin/Block/GoogleMapAllPointsBlock.php
@@ -3,6 +3,8 @@
 namespace Drupal\google_map_field\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides a 'Google Map All markers and routes' Block.
@@ -12,21 +14,32 @@ use Drupal\Core\Block\BlockBase;
  *   admin_label = @Translation("Google Map all markers and routes block"),
  * )
  */
-class GoogleMapAllPointsBlock extends BlockBase {
+class GoogleMapAllPointsBlock extends BlockBase  implements BlockPluginInterface {
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $query = \Drupal::database()->select('node__field_maps', 'nfm');
-    $query->fields('nfm', ['entity_id', 'field_maps_name', 'field_maps_lat', 'field_maps_lon', 'field_maps_zoom', 'field_maps_type', 'field_maps_marker', 'field_maps_controls', 'field_maps_width', 'field_maps_height', 'field_maps_routepairs', 'field_maps_markerpairs']);
+    $query = \Drupal::database()->select('node__field_map', 'nfm');
+    $query->fields('nfm', ['entity_id', 'field_map_name', 'field_map_lat', 'field_map_lon', 'field_map_zoom', 'field_map_type', 'field_map_marker', 'field_map_controls', 'field_map_width', 'field_map_height', 'field_map_routepairs', 'field_map_markerpairs']);
     $query->join('node_field_data', 'nfd', 'nfd.nid = nfm.entity_id');
     $query->condition('nfd.status', '1');
-
-
     $maps = $query->execute()->fetchAllAssoc('entity_id');
     $elements = [];
     $index = 0;
+    $term_list = array(
+      'all' => $this->t('All'),
+    );
+    $config = $this->getConfiguration();
+
+    if ($config['google_map_field_show_region_selector']) {
+      $vocabTerms = \Drupal::service('entity_type.manager')->getStorage('taxonomy_term')->loadByProperties([
+        'vid' => 'regions'
+      ]);
+      foreach ($vocabTerms as $term) {
+        $term_list[$term->id()] = $term->getName();
+      }
+    }
 
     $element = [
       '#theme' => 'google_map_field',
@@ -40,18 +53,85 @@ class GoogleMapAllPointsBlock extends BlockBase {
       '#width' => "100%",
       '#height' => "450px",
       '#custom_class' => 'map-all',
+      '#regions' => $term_list,
+      '#region_selector' => ($config['google_map_field_show_region_selector'] ? TRUE : FALSE),
     ];
 
+    $element['#attached']['library'][] = 'google_map_field/google-map-field-renderer';
+    $element['#attached']['library'][] = 'google_map_field/google-map-apis';
+
+    $element['#attached']['drupalSettings']['google_map_field']['all']['config']['marker'] = ($config['google_map_field_show_marker'] ? TRUE : FALSE);
+    $element['#attached']['drupalSettings']['google_map_field']['all']['config']['route'] = ($config['google_map_field_show_route'] ? TRUE : FALSE);
+    $element['#attached']['drupalSettings']['google_map_field']['all']['config']['tooltip'] = ($config['google_map_field_show_tooltip'] ? TRUE : FALSE);
+    $element['#attached']['drupalSettings']['google_map_field']['all']['config']['region_selector'] = ($config['google_map_field_show_region_selector'] ? TRUE : FALSE);
+
     foreach ($maps as $delta => $item) {
-      $element['#attached']['library'][] = 'google_map_field/google-map-field-renderer';
-      $element['#attached']['library'][] = 'google_map_field/google-map-apis';
-      $element['#attached']['drupalSettings']['google_map_field']['all']['route']['item'.$index] = $item->field_maps_routepairs;
-      $element['#attached']['drupalSettings']['google_map_field']['all']['marker']['item'.$index] = $item->field_maps_markerpairs;
-      $index++;
+
+      $stop = '';
+      $element['#attached']['drupalSettings']['google_map_field']['all']['route']['item'.$index] = $item->field_map_routepairs;
+      $element['#attached']['drupalSettings']['google_map_field']['all']['marker']['item'.$index] = $item->field_map_markerpairs;
+
+      $node = \Drupal::entityTypeManager()->getStorage('node')->load($item->entity_id);
+
+
+
+      $element['#attached']['drupalSettings']['google_map_field']['all']['regions']['item'.$index] = $node->get('field_region')->getValue()[0]['target_id'];
+        $index++;
     }
 
+    $stop = '';
     $elements[] = $element;
     return $elements;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
+    $form['google_map_field_show_marker'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Show Markers?'),
+      '#description' => $this->t('Checking this will show markers on the map'),
+      '#default_value' => isset($config['google_map_field_show_marker']) ? $config['google_map_field_show_marker'] : FALSE,
+    );
+
+    $form['google_map_field_show_route'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Show Routes?'),
+      '#description' => $this->t('Checking this will show Routes on the map'),
+      '#default_value' => isset($config['google_map_field_show_route']) ? $config['google_map_field_show_route'] : FALSE,
+    );
+
+    $form['google_map_field_show_tooltip'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Show marker tooltips?'),
+      '#description' => $this->t('Checking this will show marker tooltips on the map'),
+      '#default_value' => isset($config['google_map_field_show_tooltip']) ? $config['google_map_field_show_tooltip'] : FALSE,
+    );
+
+    $form['google_map_field_show_region_selector'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Show Region Selector?'),
+      '#description' => $this->t('Checking this will show the region selector on the map'),
+      '#default_value' => isset($config['google_map_field_show_region_selector']) ? $config['google_map_field_show_region_selector'] : FALSE,
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    parent::blockSubmit($form, $form_state);
+    $values = $form_state->getValues();
+    $this->configuration['google_map_field_show_marker'] = $values['google_map_field_show_marker'];
+    $this->configuration['google_map_field_show_route'] = $values['google_map_field_show_route'];
+    $this->configuration['google_map_field_show_tooltip'] = $values['google_map_field_show_tooltip'];
+    $this->configuration['google_map_field_show_region_selector'] = $values['google_map_field_show_region_selector'];
+  }
 }
